5297cf0a06a08cac93d9fd4a30a9655b7b47c8ba
Spencer Jack <spencer.jack@alaskaair.com>
2018-10-28 20:17:21 -0700

    adds stylistic changes, adds header

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
 let triviaGame = {
     num: 0,
     timeLeft: 30,
-
+    correctNum: 0,
+    incorrectNum: 0,
     timerId: "",
     answerClicked: false,
     questions: [{
@@ -38,13 +39,107 @@ let triviaGame = {
                 { answer: "Carbon" },
 
             ]
-        }
+        }, 
+        {
+            question: "What type of galaxy is our Milky Way?",
+            answer: "A spiral galaxy",
+            options: [
+                { answer: "An irregular galaxy" },
+                { answer: "A spiral galaxy" },
+                { answer: "An intermediate galaxy" },
+                { answer: "An elliptical galacxy" },
+            ]
+        },
+        {
+            question: "A Schwarzschild radius:",
+            answer: "Is the event horizon of a non-rotating black hole",
+            options: [{
+                    answer: "Requires a theory of quantum gravity"
+                },
+                {
+                    answer: "Is the event horizon of a non-rotating black hole"
+                },
+                {
+                    answer: "Is responsible for the redshift of ancient light"
+                },
+                {
+                    answer: "Is a biproduct of Hawking radiation"
+                },
+            ]
+        },
     ],
+
+    makeHeaderStats: function() {
+        let header = $('<div></div>');
+        header.attr('id', 'header');
+        $('#triviaGame').append(header);
+
+        let score1 = $('<div></div>');
+        score1.attr('id', 'score1');
+        header.append(score1);
+
+        let numCorrectTop = $('<div></div>');
+        numCorrectTop.attr('id', 'numCorrectTop')
+        score1.append(numCorrectTop);
+        numCorrectTop.text(`Number`);
+
+        let numCorrectBottom = $('<div></div>');
+        numCorrectBottom.attr('id', 'numCorrectBottom')
+        score1.append(numCorrectBottom);
+        numCorrectBottom.text(`Correct:`);
+
+        let correctNumSpan = $('<div></div>');
+        correctNumSpan.attr('id', 'correctNumSpan');
+        score1.append(correctNumSpan);
+        correctNumSpan.text(triviaGame.correctNum);
+
+
+
+        let score2 = $('<div></div>');
+        score2.attr('id', 'score2');
+        header.append(score2);
+
+        let numIncorrectTop = $('<div></div>');
+        numIncorrectTop.attr('id', 'numIncorrectTop')
+        score2.append(numIncorrectTop);
+        numIncorrectTop.text(`Number`);
+
+        let numIncorrectBottom = $('<div></div>');
+        numIncorrectBottom.attr('id', 'numIncorrectBottom')
+        score2.append(numIncorrectBottom);
+        numIncorrectBottom.text(`Incorrect:`);
+
+        let incorrectNumSpan = $('<div></div>');
+        incorrectNumSpan.attr('id', 'incorrectNumSpan');
+        score2.append(incorrectNumSpan);
+        incorrectNumSpan.text(triviaGame.incorrectNum);
+
+        // #timerSpan
+        let timer = $('<div></div>');
+        timer.attr('id', 'timer');
+        header.append(timer);
+
+        let timerTop = $('<div></div>');
+        timerTop.attr('id', 'timerTop')
+        timer.append(timerTop);
+        timerTop.text(`Seconds`);
+
+        let timerBottom = $('<div></div>');
+        timerBottom.attr('id', 'timerBottom')
+        timer.append(timerBottom);
+        timerBottom.text(`Remaining:`);
+
+        let timerSpan = $('<div></div>');
+        timerSpan.attr('id', 'timerSpan');
+        timer.append(timerSpan);
+        timerSpan.text(triviaGame.incorrectNum);
+    },
 
     makeGameBoard: function (num) {
         triviaGame.answerClicked = false;
         // resets the board
         $('#triviaGame').empty();
+        triviaGame.makeHeaderStats();
 
         for (var i=num; i <= num; i++) {
             // saves index of question as global 
@@ -82,31 +177,33 @@ let triviaGame = {
         triviaGame.checkAnswer();
     },
 
-
     makeTimer: function () {
         // clears the timer from DOM
-        $('#timer').remove();
+        // $('#timer').remove();
         clearTimeout(triviaGame.timerId);
         triviaGame.timeLeft = 30;
 
-        // creates DOM element
-        let timerDisplay = $('<div></div>');
-        // gives ID attribute to new DOM element
-        timerDisplay.attr('id', 'timer');
-        // appends to the DOM
-        $('#question').append(timerDisplay);
+        // // creates DOM element
+        // let timerDisplay = $('<div></div>');
+        // // gives ID attribute to new DOM element
+        // timerDisplay.attr('id', 'timer');
+        // // appends to the DOM
+        // $('#question').append(timerDisplay);
 
         triviaGame.timerId = setInterval(triviaGame.countdown, 1000);
     },
 
     countdown: function() {
         if (triviaGame.timeLeft === 0) {
-            // update points here
+            // player ran out of time, didn't answer
+            triviaGame.incorrectNum++
             triviaGame.stopTimer();
+            triviaGame.num++
+            triviaGame.makeGameBoard(triviaGame.num);
         } else if (triviaGame.answerClicked) {
             triviaGame.stopTimer();
         } else {
-            $('#timer').text(`Seconds Remaining: ${triviaGame.timeLeft}`);
+            $('#timerSpan').text(triviaGame.timeLeft);
             triviaGame.timeLeft--;
         }
     },
@@ -123,21 +220,23 @@ let triviaGame = {
             if (this.id === triviaGame.questions[triviaGame.index].answer) {
                 console.log('match found!');
                 triviaGame.num++;
+                triviaGame.correctNum++
                 triviaGame.makeTimer();
                 triviaGame.makeGameBoard(triviaGame.num);
             }else {
                 console.log('nope');
+                triviaGame.incorrectNum++
                 triviaGame.num++;
                 triviaGame.makeTimer();
                 triviaGame.makeGameBoard(triviaGame.num);
             }
         })
     },
-
 }
 
 // randomize answers 
 // create tally
     // correct, incorrect, 
+
 // create tally
     // 5/10
