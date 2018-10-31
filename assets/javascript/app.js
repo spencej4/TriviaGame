8143e17a3254d7e2185a2c75facf3771b4507d7e
Spencer Jack <spencer.jack@alaskaair.com>
2018-10-31 11:50:32 -0700

    styling changes in css. Logic updates in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,58 +4,127 @@ $(document).ready(function () {
 
 let triviaGame = {
     num: 0,
-    timeLeft: 30,
+    timeLeft: 5000,
     correctNum: 0,
     incorrectNum: 0,
+    header: "",
     timerId: "",
-    gif: "", /* new */
+    gif: "",
     answerClicked: false,
-    questions: [{
+    questions: [
+        {
+            question: "This person wanted to be a ventriloquist",
+            gifLink: "assets/images/surfing.gif",
+            answer: "Kelly_Slater",
+            options: [{
+                    answer: "David Beckham"
+                },
+                {
+                    answer: "Beyonce"
+                },
+                {
+                    answer: "Kelly Slater"
+                },
+                {
+                    answer: "Ashton Kutcher"
+                }
+            ]
+        },
+        {
+            question: "The sky is...",
+            gifLink: "assets/images/butWhy.gif",
+            answer: "Blue",
+            options: [{
+                    answer: "Green"
+                },
+                {
+                    answer: "Blue"
+                },
+                {
+                    answer: "Yellow"
+                },
+                {
+                    answer: "White"
+                }
+            ]
+        },
+        {
             question: "The rain in spain falls mainly in the plain",
-            gifLink: "assets/images/mfl.gif", /* new */
-            answer: "My Fair Lady",
-            options: [
-                { answer: "Sound of Music" },
-                { answer: "Westside Story" },
-                { answer: "My Fair Lady" },
-                { answer: "Pink Panther" }
+            gifLink: "assets/images/mfl.gif",
+            answer: "My_Fair_Lady",
+            options: [{
+                    answer: "Sound of Music"
+                },
+                {
+                    answer: "Westside Story"
+                },
+                {
+                    answer: "My Fair Lady"
+                },
+                {
+                    answer: "Pink Panther"
+                }
             ]
         },
         {
             question: "What about the animal cargo?",
-            answer: "Ace Ventura",
-            gifLink: "assets/images/ventura.gif",/* new */
-            options: [
-                { answer: "Jurassic Park" },
-                { answer: "Pulp Fiction" },
-                { answer: "Dumb and Dumber" },
-                { answer: "Ace Ventura" }
+            answer: "Ace_Ventura",
+            gifLink: "assets/images/ventura.gif",
+            options: [{
+                    answer: "Jurassic Park"
+                },
+                {
+                    answer: "Pulp Fiction"
+                },
+                {
+                    answer: "Dumb and Dumber"
+                },
+                {
+                    answer: "Ace Ventura"
+                }
             ]
         },
         {
             question: "The sun is made mostly from what element?",
             answer: "Hydrogen",
-            options: [
-                { answer: "Hydrogen" },
-                { answer: "Helium" },
-                { answer: "Nitrogen" },
-                { answer: "Carbon" },
+            gifLink: "assets/images/sun.gif",
+            options: [{
+                    answer: "Hydrogen"
+                },
+                {
+                    answer: "Helium"
+                },
+                {
+                    answer: "Nitrogen"
+                },
+                {
+                    answer: "Carbon"
+                },
 
             ]
-        }, 
+        },
         {
             question: "What type of galaxy is our Milky Way?",
-            answer: "A spiral galaxy",
-            options: [
-                { answer: "An irregular galaxy" },
-                { answer: "A spiral galaxy" },
-                { answer: "An intermediate galaxy" },
-                { answer: "An elliptical galacxy" },
+            answer: "A_spiral_galaxy",
+            gifLink: "assets/images/galaxy.gif",
+            options: [{
+                    answer: "An irregular galaxy"
+                },
+                {
+                    answer: "A spiral galaxy"
+                },
+                {
+                    answer: "An intermediate galaxy"
+                },
+                {
+                    answer: "An elliptical galacxy"
+                },
             ]
         },
         {
             question: "A Schwarzschild radius:",
-            answer: "Is the event horizon of a non-rotating black hole",
+            answer: "Is_the_event_horizon_of_a_non-rotating_black_hole",
+            gifLink: "assets/images/blackHole.gif",
             options: [{
                     answer: "Requires a theory of quantum gravity"
                 },
@@ -72,14 +141,16 @@ let triviaGame = {
         },
     ],
 
-    makeHeaderStats: function() {
-        let header = $('<div></div>');
-        header.attr('id', 'header');
-        $('#triviaGame').append(header);
+    makeHeaderStats: function () {
+        //header
+        triviaGame.header = $('<div></div>');
+        triviaGame.header.attr('id', 'header');
+        $('#triviaGame').append(triviaGame.header);
 
+        // number  of correct answers
         let score1 = $('<div></div>');
         score1.attr('id', 'score1');
-        header.append(score1);
+        triviaGame.header.append(score1);
 
         let numCorrectTop = $('<div></div>');
         numCorrectTop.attr('id', 'numCorrectTop')
@@ -97,10 +168,10 @@ let triviaGame = {
         correctNumSpan.text(triviaGame.correctNum);
 
 
-
+        // number of incorrect answers
         let score2 = $('<div></div>');
         score2.attr('id', 'score2');
-        header.append(score2);
+        triviaGame.header.append(score2);
 
         let numIncorrectTop = $('<div></div>');
         numIncorrectTop.attr('id', 'numIncorrectTop')
@@ -117,10 +188,35 @@ let triviaGame = {
         score2.append(incorrectNumSpan);
         incorrectNumSpan.text(triviaGame.incorrectNum);
 
+        //number of questions
+        let count = $('<div></div>');
+        count.attr('id', 'count');
+        triviaGame.header.append(count);
+
+        let countTop = $('<div></div>');
+        countTop.attr('id', 'countTop')
+        count.append(countTop);
+        countTop.text(`Question`);
+
+        let countBottom = $('<div></div>');
+        countBottom.attr('id', 'countBottom')
+        count.append(countBottom);
+        countBottom.text(`Count:`);
+
+        let countSpan = $('<div></div>');
+        countSpan.attr('id', 'countSpan');
+        count.append(countSpan);
+        countSpan.text(triviaGame.num + 1);
+
+        let countSpan2 = $('<div></div>');
+        countSpan2.attr('id', 'countSpan2');
+        count.append(countSpan2);
+        countSpan2.text(`/${triviaGame.questions.length}`);
+
         // #timerSpan
         let timer = $('<div></div>');
         timer.attr('id', 'timer');
-        header.append(timer);
+        triviaGame.header.append(timer);
 
         let timerTop = $('<div></div>');
         timerTop.attr('id', 'timerTop')
@@ -138,65 +234,87 @@ let triviaGame = {
         timerSpan.text(triviaGame.timeLeft);
     },
 
-    emptyBoard: function() {
-        // resets the board values
-        triviaGame.answerClicked = false;
-        $('#triviaGame').empty();
+    stopGame: function () {
+        let numLeft = triviaGame.questions.length;
+        console.log(numLeft);
+        if (numLeft === triviaGame.num) {
+            console.log('game over');
+            triviaGame.showGameOverGif();
+            triviaGame.showPlayAgain();
+        } else {
+            triviaGame.makeGameBoard(triviaGame.num);
+        }
     },
-    
-    makeGameBoard: function (num) {
-        triviaGame.emptyBoard();
-        triviaGame.makeHeaderStats();
 
-        for (var i=num; i <= num; i++) {
+    makeGameBoard: function (num) {
+        this.emptyBoard();
+        this.makeHeaderStats();
+
+        for (var i = num; i <= num; i++) {
             // saves index of question as global 
-            // used in checkAnswer function
-            triviaGame.index = i;
-            triviaGame.gif = triviaGame.questions[i].gifLink;/* new */
+            triviaGame.index = i; /* (used in checkAnswer function) */
+            triviaGame.gif = triviaGame.questions[i].gifLink;
             // creates
-            let gifBoard = $('<div></div>'); /* new */
+            let gifBoard = $('<div></div>');
             let question = $('<div></div>');
             let optionsBoard = $('<div></div>');
+            let answerColumn = $('<div></div>');
             // adds attributes
-            gifBoard.attr('id','gifBoard'); /* new */
+            gifBoard.attr('id', 'gifBoard');
             question.attr('id', 'question');
             optionsBoard.attr('id', 'optionsBoard');
+            answerColumn.attr('id', 'answerColumn');
             // appends DOM
             $('#triviaGame').append(gifBoard);
             $('#triviaGame').append(question);
             $('#triviaGame').append(optionsBoard);
+            optionsBoard.append(answerColumn);
             // sets text
             question.text(`Q: ${triviaGame.questions[i].question}`);
 
-            for (var j=0; j < triviaGame.questions[i].options.length; j++) {
+            for (var j = 0; j < triviaGame.questions[i].options.length; j++) {
                 // creates
                 let answer = $('<div></div>');
+                // joins empty spaces in answers to create ID's
+                let answerID =
+                    triviaGame.questions[i].options[j].answer.split(" ").join("_");                
                 // adds attributes
-                answer.addClass('answer').attr('id', triviaGame.questions[i].options[j].answer);
+                answer.addClass('answer').attr('id', answerID);
                 // appends DOM
-                $('#optionsBoard').append(answer);
+                $('#answerColumn').append(answer);
                 answer.text(triviaGame.questions[i].options[j].answer);
             }
             break;
         }
-        console.log(triviaGame.gif);
-        triviaGame.makeTimer();
-        triviaGame.checkAnswer();
+        this.makeTimer();
+        this.checkAnswer();
+    },
+
+    emptyBoard: function () {
+        // resets the board values
+        triviaGame.answerClicked = false;
+        $('#triviaGame').empty();
+    },
+
+    emptyHeaderStats: function () {
+        // resets header 
+        $('#header').empty();
     },
 
     makeTimer: function () {
         clearTimeout(triviaGame.timerId);
-        triviaGame.timeLeft = 30;
+        triviaGame.timeLeft = 5000;
         triviaGame.timerId = setInterval(triviaGame.countdown, 1000);
     },
 
-    countdown: function() {
+    countdown: function () {
+        // player ran out of time, didn't answer
         if (triviaGame.timeLeft === 0) {
-            // player ran out of time, didn't answer
             triviaGame.incorrectNum++
             triviaGame.stopTimer();
             triviaGame.num++
             triviaGame.makeGameBoard(triviaGame.num);
+        // player clicked an answer
         } else if (triviaGame.answerClicked) {
             triviaGame.stopTimer();
         } else {
@@ -209,60 +327,108 @@ let triviaGame = {
         clearTimeout(triviaGame.timerId);
     },
 
-    checkAnswer: function() {
-        $('.answer').click(function() {
-            if (!triviaGame.answerClicked){
+    checkAnswer: function () {
+        $('.answer').click(function () {
+            // prevents ability to choose more than one answer
+            if (!triviaGame.answerClicked) {
                 let selection = this.id;
                 triviaGame.answerClicked = true;
                 triviaGame.showGIF();
 
                 if (this.id === triviaGame.questions[triviaGame.index].answer) {
                     triviaGame.answerSelected(selection);
-                }else {
+                } else {
                     triviaGame.answerNotSelected(selection);
                 }
             }
         })
     },
 
-    showGIF: function() {
+    showGIF: function () {
         $('#gifBoard').css("background-image", "url('" + triviaGame.gif + "')");
     },
 
-    answerSelected: function(selection) {
+    showGameOverGif: function () {
+        $('#gifBoard').css("background-image", 'url("assets/images/gameOver.gif")');
+    },
+
+    showPlayAgain: function() {
+        // empties the options board
+        $('#optionsBoard').empty();
+        // creates new div (play again button)
+        let playAgain = $('<div></div>');
+        // adds id to new div
+        playAgain.attr('id', 'playAgain');
+        playAgain.text('Play Again');
+        // appends new div to DOM
+        $('#optionsBoard').append(playAgain);
+
+        $('#playAgain').click(function() {
+            triviaGame.reset();
+            triviaGame.makeGameBoard(0);
+        });
+    },
+
+    reset: function() {
+        // resets global stat values
+        triviaGame.num = 0;
+        triviaGame.correctNum = 0;
+        triviaGame.incorrectNum = 0;
+    },
+
+    answerSelected: function (selection) {
         let selected = document.getElementById(selection);
-        selected.style.backgroundColor = 'green';
+        selected.style.backgroundColor = '#C9FF7E';
+        selected.style.color = '#666';
         triviaGame.correctNum++
         $('#correctNumSpan').text(triviaGame.correctNum);
         triviaGame.num++;
         $('#question').text("Nice Job!");
 
-        myVar = setTimeout(function () { 
+        myVar = setTimeout(function () {
             triviaGame.makeTimer();
-            triviaGame.makeGameBoard(triviaGame.num);
+            // check if game is out of questions
+            triviaGame.stopGame();
+            // triviaGame.makeGameBoard(triviaGame.num);
         }, 5000);
     },
 
-    answerNotSelected: function(selection) {
-        let selected = document.getElementById(selection);
-        // selected.style.backgroundColor = '#b3b3cc';
-
-        selected.className = 'wrong';
+    answerNotSelected: function (selection) {
+        console.log(selection);
+        let selected = $("#" + selection);
+        console.log(selected);
+        // changes background of selected answer
+        selected.css('background-color', '#F33C62');
+        selected = "";
+        //updates number of incorrect
         triviaGame.incorrectNum++
+        // changes text of incorrectNum in header
         $('#incorrectNumSpan').text(triviaGame.incorrectNum);
+        // increments number of question
         triviaGame.num++;
+        // displays message
         $('#question').text("Nice try...");
 
         myVar = setTimeout(function () {
             triviaGame.makeTimer();
-            triviaGame.makeGameBoard(triviaGame.num);
+            triviaGame.stopGame();
+            // triviaGame.makeGameBoard(triviaGame.num);
         }, 5000);
     },
 }
 
-// randomize answers 
-// create tally             DONE
-    // correct, incorrect,  DONE
 
-// create tally
-    // 5/10
+
+// add more questions
+// and gifs
+
+// create game over div             DONE
+// create play again button and     DONE
+// create play again function
+
+// create tally            DONE
+// 5/10                DONE
+
+// randomize answers 
+// create tally         DONE
+// correct, incorrect,  DONE
